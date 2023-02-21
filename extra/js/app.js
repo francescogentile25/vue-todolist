cab79b3c12e30ce0a7ce5f081cd9330d9e91e8e9
francescogentile25 <francescogentile250893@gmail.com>
2023-02-21 15:43:32 +0100

    FEAT add enter keydown

---
+++ extra/js/app.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue
+
+createApp({
+	data() {
+		return {
+			tasks: [
+                {
+                    text: 'Fare la spesa',
+                    done: true,
+                },
+                {
+                    text: 'Mandare la consegna alla classe',
+                    done: false,
+                },
+                {
+                    text:'Conquistare il mondo',
+                    done: true,
+                },
+                {
+                    text: 'Fare push',
+                    done: true,
+                },
+			],
+			inputValue: '',
+		}
+	},
+	methods: {
+		addTask() {
+			// console.log('add task:', this.inputValue)
+            
+			const newTask = this.tasks.push({text:this.inputValue, done:false})
+			
+			this.inputValue = ''
+		},
+		removeTask(startIndex) {
+			console.log(
+				'elimino il task con indice:',
+				startIndex,
+				this.tasks[startIndex]
+			)
+			this.tasks.splice(startIndex, 1)
+		},
+		saluta(){
+			console.log('ciao') 
+			
+		}
+	},
+}).mount('#app')
+
